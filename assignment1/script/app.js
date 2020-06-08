bebe5e301a99bd4308e4e7e229c3fc7529e437c7
peter.pavucok <peter.pavucok@t-systems.com>
2020-06-08 23:10:29 +0200

    Adding style to the textbox with the warning.

--- assignment1/script/app.js
+++ assignment1/script/app.js
@@ -7,7 +7,7 @@
     function LunchCheckController($scope) {
 
       $scope.writtenString = "";
-
+      $scope.textStyle = {color: 'black', border: 'none'};
       $scope.listOfMyStrings = function () {
         $scope.listings = $scope.writtenString.split(',');
         removeWhitespacesFromItems();
@@ -33,8 +33,10 @@
       $scope.showWarning = function () {
         $scope.listOfMyStrings();
         if ($scope.listings.length === 0) {
+          $scope.textStyle = {'color': 'red','border': '1px solid red'};
           $scope.warning = "Please enter data first";
         } else {
+          $scope.textStyle = {'color': 'green', 'border': '1px solid green'};
           if ($scope.listings.length <= 3 && $scope.listings.length > 0) {
             $scope.warning = "Enjoy";
           } else if ($scope.listings.length > 3) {
